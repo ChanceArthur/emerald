8631f61891d4c209fcdcb023a943fb82190671a8
Chance Arthur <23122071+ChanceArthur@users.noreply.github.com>
2026-03-31 18:01:24 -0400

    Uncomment BUGFIX definition in config.h

--- include/config.h
+++ include/config.h
@@ -44,8 +44,7 @@
 #define CHAR_DEC_SEPARATOR CHAR_COMMA
 #endif
 
-// Uncomment to fix some identified minor bugs
-//#define BUGFIX
+#define BUGFIX
 
 // Various undefined behavior bugs may or may not prevent compilation with
 // newer compilers. So always fix them when using a modern compiler.
